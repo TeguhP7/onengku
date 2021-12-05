8897cd52bc5105cdcaccbbb982ae451ad6e841af
Teguh Pamungkas <tpamungkas25@gmail.com>
2021-12-05 07:33:49 +0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -50,9 +50,9 @@ const animationTimeline = () => {
   const tl = new TimelineMax();
 
   tl.to(".container", 0.1, {
-      visibility: "visible",
-    })
-    .from(".one", 2, {
+    visibility: "visible",
+  })
+    .from(".one", 0.7, {
       opacity: 0,
       y: 10,
     })
@@ -62,18 +62,18 @@ const animationTimeline = () => {
     })
     .to(
       ".one",
-      4, {
-        opacity: 0,
-        y: 10,
-      },
+      0.7, {
+      opacity: 0,
+      y: 10,
+    },
       "+=2.5"
     )
     .to(
       ".two",
       0.7, {
-        opacity: 0,
-        y: 10,
-      },
+      opacity: 0,
+      y: 10,
+    },
       "-=1"
     )
     .from(".three", 0.7, {
@@ -84,9 +84,9 @@ const animationTimeline = () => {
     .to(
       ".three",
       0.7, {
-        opacity: 0,
-        y: 10,
-      },
+      opacity: 0,
+      y: 10,
+    },
       "+=2"
     )
     .from(".four", 0.7, {
@@ -100,8 +100,8 @@ const animationTimeline = () => {
     .staggerTo(
       ".hbd-chatbox span",
       1.5, {
-        visibility: "visible",
-      },
+      visibility: "visible",
+    },
       0.05
     )
     .to(".fake-btn", 0.1, {
@@ -110,10 +110,10 @@ const animationTimeline = () => {
     .to(
       ".four",
       0.5, {
-        scale: 0.2,
-        opacity: 0,
-        y: -150,
-      },
+      scale: 0.2,
+      opacity: 0,
+      y: -150,
+    },
       "+=0.7"
     )
     .from(".idea-1", 0.7, ideaTextTrans)
@@ -133,72 +133,72 @@ const animationTimeline = () => {
     .from(
       ".idea-5",
       0.7, {
-        rotationX: 15,
-        rotationZ: -10,
-        skewY: "-5deg",
-        y: 50,
-        z: 10,
-        opacity: 0,
-      },
+      rotationX: 15,
+      rotationZ: -10,
+      skewY: "-5deg",
+      y: 50,
+      z: 10,
+      opacity: 0,
+    },
       "+=0.5"
     )
     .to(
       ".idea-5 span",
       0.7, {
-        rotation: 90,
-        x: 8,
-      },
+      rotation: 90,
+      x: 8,
+    },
       "+=0.4"
     )
     .to(
       ".idea-5",
       0.7, {
-        scale: 0.2,
-        opacity: 0,
-      },
+      scale: 0.2,
+      opacity: 0,
+    },
       "+=2"
     )
     .staggerFrom(
       ".idea-6 span",
       0.8, {
-        scale: 3,
-        opacity: 0,
-        rotation: 15,
-        ease: Expo.easeOut,
-      },
+      scale: 3,
+      opacity: 0,
+      rotation: 15,
+      ease: Expo.easeOut,
+    },
       0.2
     )
     .staggerTo(
       ".idea-6 span",
       0.8, {
-        scale: 3,
-        opacity: 0,
-        rotation: -15,
-        ease: Expo.easeOut,
-      },
+      scale: 3,
+      opacity: 0,
+      rotation: -15,
+      ease: Expo.easeOut,
+    },
       0.2,
       "+=1"
     )
     .staggerFromTo(
       ".baloons img",
       2.5, {
-        opacity: 0.9,
-        y: 1400,
-      }, {
-        opacity: 1,
-        y: -1000,
-      },
+      opacity: 0.9,
+      y: 1400,
+    }, {
+      opacity: 1,
+      y: -1000,
+    },
       0.2
     )
     .from(
       ".lydia-dp",
       0.5, {
-        scale: 3.5,
-        opacity: 0,
-        x: 25,
-        y: -25,
-        rotationZ: -45,
-      },
+      scale: 3.5,
+      opacity: 0,
+      x: 25,
+      y: -25,
+      rotationZ: -45,
+    },
       "-=2"
     )
     .from(".hat", 0.5, {
@@ -210,47 +210,47 @@ const animationTimeline = () => {
     .staggerFrom(
       ".wish-hbd span",
       0.7, {
-        opacity: 0,
-        y: -50,
-        // scale: 0.3,
-        rotation: 150,
-        skewX: "30deg",
-        ease: Elastic.easeOut.config(1, 0.5),
-      },
+      opacity: 0,
+      y: -50,
+      // scale: 0.3,
+      rotation: 150,
+      skewX: "30deg",
+      ease: Elastic.easeOut.config(1, 0.5),
+    },
       0.1
     )
     .staggerFromTo(
       ".wish-hbd span",
       0.7, {
-        scale: 1.4,
-        rotationY: 150,
-      }, {
-        scale: 1,
-        rotationY: 0,
-        color: "#ff69b4",
-        ease: Expo.easeOut,
-      },
+      scale: 1.4,
+      rotationY: 150,
+    }, {
+      scale: 1,
+      rotationY: 0,
+      color: "#ff69b4",
+      ease: Expo.easeOut,
+    },
       0.1,
       "party"
     )
     .from(
       ".wish h5",
       0.5, {
-        opacity: 0,
-        y: 10,
-        skewX: "-15deg",
-      },
+      opacity: 0,
+      y: 10,
+      skewX: "-15deg",
+    },
       "party"
     )
     .staggerTo(
       ".eight svg",
       1.5, {
-        visibility: "visible",
-        opacity: 0,
-        scale: 80,
-        repeat: 3,
-        repeatDelay: 1.4,
-      },
+      visibility: "visible",
+      opacity: 0,
+      scale: 80,
+      repeat: 3,
+      repeatDelay: 1.4,
+    },
       0.3
     )
     .to(".six", 0.5, {
@@ -262,8 +262,8 @@ const animationTimeline = () => {
     .to(
       ".last-smile",
       0.5, {
-        rotation: 90,
-      },
+      rotation: 90,
+    },
       "+=1"
     );
 
